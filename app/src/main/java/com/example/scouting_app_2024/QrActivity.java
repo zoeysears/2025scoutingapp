f7600c1c9d5feccb45f36362e6bca08ea14b4a69
AamirAbatiyow <150764416+AamirAbatiyow@users.noreply.github.com>
2024-02-10 22:53:35 -0600

    fixed all issues!

--- app/src/main/java/com/example/scouting_app_2024/QrActivity.java
+++ app/src/main/java/com/example/scouting_app_2024/QrActivity.java
@@ -1,17 +1,38 @@
 package com.example.scouting_app_2024;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.qrgenearator.QRGEncoder;
+import com.example.qrgenearator.QRGContents;
+
 public class QrActivity extends AppCompatActivity {
+    QRGEncoder qrgEncoder;
+    Bitmap bitmap;
+    ImageView qrImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
         String json = RecordsActivity.Info.CreateJSON();
+        qrImage = findViewById(R.id.qrImageView);
+        qrgEncoder = new QRGEncoder(json, null, QRGContents.Type.TEXT, 200);
+        try {
+            // Getting QR-Code as Bitmap
+            qrgEncoder.setColorBlack(this.getResources().getColor(R.color.darkGrey));
+            qrgEncoder.setColorWhite(this.getResources().getColor(R.color.yellow));
+            bitmap = qrgEncoder.getBitmap();
+            qrImage.setImageBitmap(bitmap);
+            // Setting Bitmap to ImageView
+            qrImage.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
